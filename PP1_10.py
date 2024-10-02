58685c29649ced9696fed060732fbae34ca3767d
Owais-Khan-ICD2O1-01 <owaisali.khan@student.tdsb.on.ca>
2024-10-02 13:31:33 +0000

    Answer

--- PP1_10.py
+++ PP1_10.py
@@ -1,31 +1,38 @@
-
+import math
 
 def q1(): 
-  #Write Assignment code here
-
+  num = float(input("Input a number: "))
+  print(math.sqrt(num))
 
 def q2(): 
-  #Write Assignment code here
+  num = float(input("Input a number: "))
+  print(math.isqrt(num))
 
 
 def q3(): 
-  #Write Assignment code here
+  num = float(input("Input a number: "))
+  print(math.floor(num))
 
 
 def q4(): 
-  #Write Assignment code here
-
+  num = float(input("Input a number: "))
+  print(math.ceil(num))
 
 def q5(): 
-  #Write Assignment code here
+  num = float(input("Input a number: "))
+  num2 = float(input("Input another number: "))
+  num *= num2
+  num = math.trunc(num/2)
+  print(num)
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
-
+'''
 q1()
 q2()
 q3()
 q4()
 q5()
+'''
